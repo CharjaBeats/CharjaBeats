e97585a3ca62c4c9da3d670a2e67dbebec7dd8b7
ChajaBeats <owen.radley+github@outlook.com>
2020-09-18 21:21:16 -0700

    Did stuff

--- Charjabug.js
+++ Charjabug.js
@@ -27,11 +27,7 @@ if(command === "charja"){
     message.channel.send("no")
 }else if(command === "rules"){
     message.channel.send("1. No verbal abuse 2. Please no spamming 3. If you are planning on inviting someone, please notify me first 4. Inappropriate images and photos will result in a kick 5. joining Jolt Skifts without permission will result in two warnings 6. Three warnings will result in a kick 7. Do not make fun of other people's skin in Jolt Plains unless they actually want to be 8. Do not gang up on other users 9. do not spam commands")
-}else if(command === "play"){
- 
-    message.channel.send("test")
-
-}
+};
 
 });
   
